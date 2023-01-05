600e58a6aa7313ce858022697125151847da86a4
PAULO SERGIO <110654467+paulosergio4@users.noreply.github.com>
2023-01-05 03:26:48 -0300

    APRENDI A USAR FUNÇÕES

---
+++ aula16/funcao2.js
@@ -0,0 +1,6 @@
+function soma( n1, n2){
+    return n1+n2
+}
+/* Existe uma função no js quando declarar uma variaél que é vc igualar a 0 (n1=0), pois assim quando voce nao declarar uma variavel o js irá considerar o 0 e nao  indefinido e retornar NaN.
+*/
+console.log (soma(2, 5))
